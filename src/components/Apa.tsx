37343507c7a226f757bfa0d3f90927e6ad8751a3
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-11-17 14:27:52 +0100

    feat: adding jest

--- src/components/Apa.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-interface ApaProps {}
-
-const Apa: React.FC<ApaProps> = ({}) => {
-  return <h1>Apa</h1>
-}
-export default Apa
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,10 @@ import React from "react"
 import { PageProps } from "gatsby"
 
 import Title from "@/components/Title"
-import Apa from "@/components/Apa"
 
 const Home: React.FC<PageProps> = () => (
   <main>
     <Title />
-
-    <Apa />
   </main>
 )
 
