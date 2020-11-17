7a8b5340f4ffa6573607107c145e93c5c5d42541
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-11-17 14:30:12 +0100

    feat: alias

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-alias-imports`,
+      options: {
+        aliases: {
+          styles: `src/styles`,
+          config: `config/`,
+          "@utils": "src/utils/"
+        }
+      }
+    }
+  ]
 };
