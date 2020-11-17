8baae9840a72314a87013c8d7aa28a96232d13a3
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-11-17 14:27:20 +0100

    adding tests

---
+++ src/__tests__/HomePage.test.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import HomePage from "../pages/index"
+import { render, screen } from "@testing-library/react"
+
+describe("", () => {
+  test("should ", () => {
+    render(<HomePage />)
+
+    screen.debug()
+  })
+})
---
+++ src/components/Apa.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+interface ApaProps {}
+
+const Apa: React.FC<ApaProps> = ({}) => {
+  return <h1>Apa</h1>
+}
+export default Apa
