799c728bcaeeafbdb10b35a7560504e160012f2d
masiucd <ciszekmarcell@gmail.com>
2021-06-19 20:58:42 +0200

    let-s start over

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import {AppProps} from "next/app"
+
+function App({Component, pageProps}: AppProps) {
+  return (
+    <>
+      {/* Globalstyles here */}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
---
+++ pages/_document.tsx
@@ -0,0 +1,118 @@
+// import Document, {
+//   Html,
+//   Head,
+//   Main,
+//   NextScript,
+//   DocumentContext,
+// } from "next/document"
+// import * as React from "react"
+// import {renderStatic} from "../shared/renderer"
+// export default class AppDocument extends Document {
+//   static async getInitialProps(ctx: DocumentContext) {
+//     const page = await ctx.renderPage()
+//     const {css, ids} = await renderStatic(page.html)
+//     const initialProps = await Document.getInitialProps(ctx)
+//     return {
+//       ...initialProps,
+//       styles: (
+//         <React.Fragment>
+//           {initialProps.styles}
+//           <style
+//             data-emotion={`css ${ids.join(" ")}`}
+//             dangerouslySetInnerHTML={{__html: css}}
+//           />
+//         </React.Fragment>
+//       ),
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head />
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     )
+//   }
+// }
+
+import Document, {Html, Head, Main, NextScript, DocumentContext} from "next/document"
+import createEmotionServer from "@emotion/server/create-instance"
+import {cache} from "@emotion/css"
+
+const {extractCritical} = createEmotionServer(cache)
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = extractCritical(initialProps.html)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion-css={styles.ids.join(" ")}
+            dangerouslySetInnerHTML={{__html: styles.css}}
+          />
+        </>
+      ),
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="preload"
+            href="/fonts/inter-var-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/fira-code.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/static/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/static/favicons/favicon-192x192.png"
+            rel="icon"
+            sizes="192x192"
+            type="image/png"
+          />
+
+          <link
+            href="/static/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/static/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
